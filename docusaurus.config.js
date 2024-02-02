b44fd1c0b5a2365b430729a9169811e92bb58905
jinhojang6 <jinho5051@gmail.com>
2024-02-02 22:25:27 +0900

    chore: update terms and privacy policy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,6 +129,10 @@ const config = {
                 href: '/terms',
                 label: 'Terms & conditions',
               },
+              {
+                href: '/privacy-policy',
+                label: 'Privacy Policy',
+              },
             ],
           },
         ],
