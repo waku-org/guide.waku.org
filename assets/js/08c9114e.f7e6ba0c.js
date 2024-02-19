dde61688147d5288073b0621c08dab9d12a50130
Jenkins <jenkins@status.im>
2024-02-19 15:19:14 +0000

    Update documentation

---
+++ assets/js/08c9114e.f7e6ba0c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklogos_docs_template=self.webpackChunklogos_docs_template||[]).push([[9350],{83769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
---
+++ assets/js/ea67b4e1.e3be49fa.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklogos_docs_template=self.webpackChunklogos_docs_template||[]).push([[8667],{28958:e=>{e.exports=JSON.parse('{"title":"Visual Language","slug":"/category/visual-language","permalink":"/category/visual-language","navigation":{"previous":{"title":"Voice","permalink":"/voice/"},"next":{"title":"Overview","permalink":"/visual-language/overview"}}}')}}]);
